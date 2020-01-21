55a656720f2a17244944c5c45f8f6b0d0e6606d9
Sillascs <59029146+Sillascs@users.noreply.github.com>
2020-01-21 13:13:24 -0300

    Conclusão do projeto ViagensMVC

--- ViagensMVC/ViagensMVC/Db/ViagensOnlineDb.cs
+++ ViagensMVC/ViagensMVC/Db/ViagensOnlineDb.cs
@@ -9,12 +9,13 @@ namespace ViagensMVC.Db
 {
     public class ViagensOnlineDb : DbContext
     {
-        private const string conexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\di3911\Documents\Unidas2020\Exercicio-Lab-Impacta\ViagensMVC\ViagensMVC\App_Data\ViagensOnlineDb.mdf;Integrated Security=True";
+        private const string conexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\di3911\Documents\Unidas2020\Exercicio-Lab-Impacta\ViagensMVC\ViagensMVC\App_Data\ViagensMvcDb.mdf;Integrated Security=True";
 
         public ViagensOnlineDb() : base(conexao)
         {
         }
 
         public DbSet<Destino> Destinos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
